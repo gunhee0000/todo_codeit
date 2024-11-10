aea2a9ca7fb551cbc7917d445d2068f94f010a05
gunhee0000 <132315752+gunhee0000@users.noreply.github.com>
2024-11-10 19:44:21 +0900

    use_firebase_auth 날린부분 복구

--- components/google_login_button.tsx
+++ components/google_login_button.tsx
@@ -8,24 +8,29 @@ interface Props {
 export const GoogleLoginButton = function ({ onClick }: Props) {
   const signInWithGoogleBtn = (
     <Button
-      size="lg"
-      width="full"
-      maxW="md"
-      borderRadius="full"
-      bgColor="#4285f4"
-      color="white"
-      colorScheme="blue"
+      sx={{
+        size: 'lg',
+        width: 'full',
+        maxW: 'md',
+        borderRadius: 'full',
+        bgColor: '#4285f4',
+        color: 'white',
+        colorScheme: 'blue',
+      }}
       leftIcon={
-        <img
-          src="/google.svg"
-          alt="google logo"
-          style={{ backgroundColor: 'white', padding: '8px', borderRadius: '30px' }}
-        />
+        <Box bg="white" p="8px" borderRadius="full">
+          <img src="/google.svg" alt="google logo" style={{ width: '20px', height: '20px' }} />
+        </Box>
       }
       onClick={onClick}
     >
       google 계정으로 시작하기
     </Button>
+    //eslint error 반복
+    //Expression produces a union type that is too complex to represent.
+    //여러가지 방법 적용해 봤지만 해결X
+    //typescript와 ChakraUI 버전 충돌로 예상
+    //버전 변경 시도X 배포 전 확인 할 것.
   );
 
   const { loading, authUser } = useAuth();
