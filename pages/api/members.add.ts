122db33e473836c22b9e04fb1f689ba25922fa5c
gunhee0000 <132315752+gunhee0000@users.noreply.github.com>
2024-11-10 22:53:27 +0900

    member model 생성

--- pages/api/members.add.ts
+++ pages/api/members.add.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 import { NextApiRequest, NextApiResponse } from 'next';
-import FirebaseAdmin from '@/models/firebase_admin';
+import memberModel from '@/models/member/member.model';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { uid, email, displayName, photoURL } = req.body;
@@ -12,45 +12,55 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return res.status(400).json({ result: false, message: 'email이 누락되었습니다.' });
   }
 
-  try {
-    const addData = {
-      uid,
-      email,
-      displayName: displayName ?? '', //존재하지 않으면 빈string
-      photoURL: photoURL ?? '',
-    };
+  /* 반복 사용 부분 -> /models/member/member.model.ts에 member model 생성 */
 
-    // const addReault = await FirebaseAdmin.getInstance()
-    //   .Firebase.collection('members')
-    //   .doc(uid)
-    //   .set({addData});
+  // try {
+  //   const addData = {
+  //     uid,
+  //     email,
+  //     displayName: displayName ?? '', //존재하지 않으면 빈string
+  //     photoURL: photoURL ?? '',
+  //   };
 
-    const screenName = (email as string).replace('@gmail.com', ''); //@gmail.com을 제거 (''으로 replace)
-    // await FirebaseAdmin.getInstance()
-    //   .Firebase.collection('screen_names')
-    //   .doc(screenName)
-    //   .set({addData});
+  //   /* Transaction으로 변경 */
 
-    const addResult = await FirebaseAdmin.getInstance().Firebase.runTransaction(async (transaction) => {
-      const memberRef = FirebaseAdmin.getInstance().Firebase.collection('members').doc(uid);
-      const screenNameRef = FirebaseAdmin.getInstance().Firebase.collection('screen_names').doc(screenName);
+  //   // const addReault = await FirebaseAdmin.getInstance()
+  //   //   .Firebase.collection('members')
+  //   //   .doc(uid)
+  //   //   .set({addData});
 
-      const memberDoc = await transaction.get(memberRef);
-      if (memberDoc.exists) {
-        //이미 추가된 상태
-        return false;
-      }
-      await transaction.set(memberRef, addData);
-      await transaction.set(screenNameRef, addData);
-      return true;
-    });
+  //   const screenName = (email as string).replace('@gmail.com', ''); //@gmail.com을 제거 (''으로 replace)
+  //   // await FirebaseAdmin.getInstance()
+  //   //   .Firebase.collection('screen_names')
+  //   //   .doc(screenName)
+  //   //   .set({addData});
 
-    if (addResult === false) {
-      return res.status(201).json({ result: true, id: uid });
-    }
-    return res.status(200).json({ result: true, id: uid });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ result: false });
+  //   const addResult = await FirebaseAdmin.getInstance().Firebase.runTransaction(async (transaction) => {
+  //     const memberRef = FirebaseAdmin.getInstance().Firebase.collection('members').doc(uid);
+  //     const screenNameRef = FirebaseAdmin.getInstance().Firebase.collection('screen_names').doc(screenName);
+
+  //     const memberDoc = await transaction.get(memberRef);
+  //     if (memberDoc.exists) {
+  //       //이미 추가된 상태
+  //       return false;
+  //     }
+  //     await transaction.set(memberRef, addData);
+  //     await transaction.set(screenNameRef, addData);
+  //     return true;
+  //   });
+
+  //   if (addResult === false) {
+  //     return res.status(201).json({ result: true, id: uid });
+  //   }
+  //   return res.status(200).json({ result: true, id: uid });
+  // } catch (err) {
+  //   console.error(err);
+  //   res.status(500).json({ result: false });
+  // }
+
+  const addReault = await memberModel.add({ uid, email, displayName, photoURL });
+  if (addReault.result === true) {
+    return res.status(200).json(addReault);
   }
+  res.status(500).json(addReault);
 }
