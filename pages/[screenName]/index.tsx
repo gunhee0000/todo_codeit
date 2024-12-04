f4bd89cb5b364d70cae31e46701faaa833c537e2
gunhee0000 <132315752+gunhee0000@users.noreply.github.com>
2024-12-04 21:33:30 +0900

    사용자 api추가, axios 설치

--- pages/[screenName]/index.tsx
+++ pages/[screenName]/index.tsx
@@ -1,35 +1,43 @@
-import { NextPage } from 'next';
-import { Avatar, Box, Button, Flex, Text, Textarea, useToast } from '@chakra-ui/react';
+import { GetServerSideProps, NextPage } from 'next';
+import { Avatar, Box, Button, Flex, FormControl, FormLabel, Switch, Text, Textarea, useToast } from '@chakra-ui/react';
 import ResizeTextarea from 'react-textarea-autosize';
 import { useState } from 'react';
+import axios, { AxiosResponse } from 'axios';
 import { ServiceLayout } from '@/components/service_layout';
+import { useAuth } from '@/context/auth_user.context';
+import { InAuthUser } from '@/models/in_auth_user';
 
-/** 임시유저 정보 */
-const userInfo = {
-  uid: 'test',
-  email: 'test@gmail.com',
-  displayname: 'tester',
-  photoURL: 'https://lh3.googleusercontent.com/a/ACg8ocIB9wvvmeNo7XGLtdBb4DwNmQbHn5qWcvgl_Lrnmqew-o27zg=s96-c',
-};
+interface Props {
+  userInfo: InAuthUser | null;
+}
 
-const UserHomePage: NextPage = function () {
+const UserHomePage: NextPage<Props> = function ({ userInfo }) {
   const [message, setMessage] = useState('');
+  const [isAnonymous, setAnonymous] = useState(true);
   const toast = useToast();
+  const { authUser } = useAuth();
+  if (userInfo === null) {
+    return <p>사용자를 찾을 수 없습니다.</p>;
+  }
   return (
     <ServiceLayout title="user home" minH="100Wh" backgroundColor="gray.50">
       <Box maxW="md" mx="auto" pt="6">
         <Box borderWidth="1px" borderRadius="lg" overflow="hidden" mb="2" bg="white">
           <Flex p="6">
-            <Avatar size="lg" src={userInfo.photoURL} mr="2" />
+            <Avatar size="lg" src={userInfo.photoURL ?? 'https://bit.ly/broken-link'} mr="2" />
             <Flex direction="column" justify="center">
-              <Text fontSize="md">{userInfo.displayname}</Text>
+              <Text fontSize="md">{userInfo.displayName}</Text>
               <Text fontSize="xs">{userInfo.email}</Text>
             </Flex>
           </Flex>
         </Box>
         <Box borderWidth="1px" borderRadius="lg" overflow="hidden" mb="2" bg="white">
           <Flex align="center" p="2">
-            <Avatar size="xs" mr="2" src="https://bit.ly/broken-link" />
+            <Avatar
+              size="xs"
+              mr="2"
+              src={isAnonymous ? 'https://bit.ly/broken-link' : authUser?.photoURL ?? 'https://bit.ly/broken-link'}
+            />
             <Textarea
               bg="gray.100"
               border="none"
@@ -67,10 +75,59 @@ const UserHomePage: NextPage = function () {
               등록
             </Button>
           </Flex>
+          <FormControl display="flex" alignItems="center" mt="1" mx="2" pb="2">
+            <Switch
+              size="sm"
+              colorScheme="orange"
+              id="anonymous"
+              mr="1"
+              isChecked={isAnonymous}
+              onChange={() => {
+                if (authUser === null) {
+                  toast({ title: '로그인이 필요합니다.', position: 'top-right' });
+                  return;
+                }
+                setAnonymous((prev) => !prev); //기존값 사용
+              }}
+            />
+            <FormLabel htmlFor="anonymous" mb="0" fontSize="xx-small">
+              Anonymous
+            </FormLabel>
+          </FormControl>
         </Box>
       </Box>
     </ServiceLayout>
   );
+};
+
+export const getServerSideProps: GetServerSideProps<Props> = async ({ query }) => {
+  const { screenName } = query;
+  if (screenName === undefined) {
+    return {
+      props: {
+        userInfo: null,
+      },
+    };
+  }
+  try {
+    const protocol = process.env.PROTOCOL || 'http';
+    const host = process.env.HOST || 'localhost';
+    const port = process.env.PORT || '3000';
+    const baseUrl = `${protocol}://${host}:${port}`;
+    const userInfoResp: AxiosResponse<InAuthUser> = await axios(`${baseUrl}/api/user.info/${screenName}`);
+    return {
+      props: {
+        userInfo: userInfoResp.data ?? null,
+      },
+    };
+  } catch (err) {
+    console.error(err);
+    return {
+      props: {
+        userInfo: null,
+      },
+    };
+  }
 };
 
 export default UserHomePage;
